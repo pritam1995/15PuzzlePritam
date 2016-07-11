012f1a68bbeb392639eb4293497bf9debf76cbfb
Pritam <pritamghosh878@gmail.com>
2016-07-11 20:34:48 +0530

    Add Timer .... Add Positive Button....Change Luncher Icon...Now able to save the state of the game

--- app/src/main/java/com/example/lab5_5/a15puzzlepritam/MainActivity.java
+++ app/src/main/java/com/example/lab5_5/a15puzzlepritam/MainActivity.java
@@ -1,23 +1,30 @@
 package com.example.lab5_5.a15puzzlepritam;
 
 import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.concurrent.RunnableFuture;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private Button[][]buttons=new Button[4][4];
+    int i;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        SharedPreferences sp=getPreferences(MODE_PRIVATE);
 
         List<Integer> l=new ArrayList<>();
         for(int i=1;i<=15;i++)
@@ -35,11 +42,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         for(int i=0;i<4;i++)
             for(int j=0;j<4;j++){
                 buttons[i][j]=new Button(this);
-                buttons[i][j].setText(l.get(k++)+ "");
+                String val=sp.getString(i +" "+ j,l.get(k++)+ "");//two args...key and default value
+                buttons[i][j].setText(val);
                 buttons[i][j].setLayoutParams(ll);
                 buttons[i][j].setOnClickListener(this);
                 buttons[i][j].setTag(i+" "+j);
             }
+        if(buttons[3][3].getText().equals("0"))
         buttons[3][3].setText("");
 
         LinearLayout rows=new LinearLayout(this);
@@ -54,6 +63,37 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             for(int j=0;j<4;j++)
                 row.addView(buttons[i][j]);
         }
+
+        LinearLayout row=new LinearLayout(this);
+        TextView tvTimerText=new TextView(this);
+        tvTimerText.setText("Elapsed Time: ");
+        row.addView(tvTimerText);
+        final TextView tvTimer=new TextView(this);
+        row.addView(tvTimer);
+        rows.addView(row);
+
+        new Thread(){
+            @Override
+            public void run() {
+               // super.run();
+                try {
+                    for ( i = 1; true; i++) {
+                        Thread.sleep(1000);
+                        runOnUiThread(
+                                new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        tvTimer.setText(i + "");
+                                    }
+                                }
+                        );
+                    }
+                }
+                    catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }.start();
 
         setContentView(rows);
     }
@@ -78,19 +118,32 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 break;
             }
         }
-        int p=1;
-        for(int i=0;i<4;i++)
-            for(int j=0;j<4;j++){
-                if(buttons[i][j].getText().equals(p++)&&buttons[3][3].getText().equals(""))
-                {new AlertDialog.Builder(this)
-                        .setMessage("Success")
-                        .show();
-                break;
-                }
+    }
+
+    @Override
+    public void onBackPressed() {
+        //super.onBackPressed(); //terminating the application by calling super classes
+        //Shared Preferences
+        //key(string) and value pairs
+        new AlertDialog.Builder(this)
+                .setTitle("save Dialog")
+                .setMessage("would u like to save?")
+                .setPositiveButton("ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        SharedPreferences sp=getPreferences(MODE_PRIVATE);
+                        SharedPreferences.Editor e=sp.edit();
 
 
-            }
+                        for(int i=0;i<4;i++)
+                            for(int j=0;j<4;j++)
+                                e.putString(i+" "+j,buttons[i][j].getText().toString());
+                                e.commit();
 
+                        finish();
+                    }
+                })
+                .show();
 
     }
 }
